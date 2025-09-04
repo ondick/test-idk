d49472e2533127ad6fc5642da94ad68baa3bd4c0
ondick <musond12@gasos-ro.cz>
2025-09-04 10:11:52 +0200

    lol

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+            ArrayList<Mazlicek> list = new ArrayList<>();
+            Scanner sc = new Scanner(System.in);
+            boolean power = true;
+            while (power = true) {
+                System.out.println("1: Pridat mazlicka");
+                System.out.println("2: Vypsat mazlicky");
+                System.out.println("3: Ukončit program");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Kolik chces pridat mazlicku?");
+                        int cislo = sc.nextInt();
+                        for (int i = 0; i < cislo; i++) {
+                            System.out.println("Jak se jmenuje?");
+                            String jmeno1 = sc.nextLine();
+                            System.out.println("Jakýje to druh?");
+                            String druh1 = sc.nextLine();
+                            System.out.println("Kolik mu je?");
+                            int vek1 = sc.nextInt();
+                            System.out.println("Kolik váží?");
+                            double vaha1 = sc.nextDouble();
+                            list.add(new Mazlicek(jmeno1,druh1,vek1,vaha1));
+                        }
+                        break;
+                    case 2:
+                        list.toString();
+                        break;
+                    case 3:
+                        System.out.println("Konec programu.");
+                        power = false;
+
+                        break;
+
+
+
+                }
+            }
+            }
+
+
+        /*List<Integer> list = new ArrayList<>(); Collections. addAll(list, 1, 2, 3, 4, 5); And another one to exemplify the operation with two arrays:
+        List<Integer> list = new ArrayList<>(); Integer[] otherList = new Integer[] {1, 2, 3, 4, 5}; Collections.*/
+        }
+
---
+++ src/Mazlicek.java
@@ -0,0 +1,50 @@
+public class Mazlicek {
+    private String jmeno;
+    private String druh;
+    private int vek;
+    private double vaha;
+
+    public Mazlicek(String jmeno, String druh, int vek, double vaha) {
+        this.jmeno = jmeno;
+        this.druh = druh;
+        this.vek = vek;
+        this.vaha = vaha;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getDruh() {
+        return druh;
+    }
+
+    public int getVek() {
+        return vek;
+    }
+
+    public double getVaha() {
+        return vaha;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public void setDruh(String druh) {
+        this.druh = druh;
+    }
+
+    public void setVek(int vek) {
+        this.vek = vek;
+    }
+
+    public void setVaha(double vaha) {
+        this.vaha = vaha;
+    }
+
+    @Override
+    public String toString() {
+        return "Jméno:"+ jmeno + ", Druh: " + druh + ", Vek: " + vek + ", Vaha: " + vaha + "kg.";
+    }
+}
